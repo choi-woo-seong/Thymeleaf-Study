6e6384b57ff216476ea172d2c6ccfa43e282604f
choi-woo-seong <goqus903@naver.com>
2025-02-06 12:19:08 +0900

    -스프링부트 타임리프 테스트

---
+++ src/main/java/my/app/first_Project/dto/UserData.java
@@ -0,0 +1,13 @@
+package my.app.first_Project.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+public class UserData {
+    private String userName;
+    private int userAge;
+    private String userAddress;
+
+}
